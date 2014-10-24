9749bb1152a5e8ec55fa6321d3c83b3f20bbb3ca
Dalimil <dalimilhajek@seznam.cz>
2014-10-24 20:17:22 +0200

    Zpracování JSON formátu odpovědi

--- Kepler/app/src/main/java/gjk/kepler/Content.java
+++ Kepler/app/src/main/java/gjk/kepler/Content.java
@@ -15,6 +15,10 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class Content extends Fragment {
 
     public static final String ARG_CONTENT_NUMBER = "content_number";
@@ -22,6 +26,9 @@ public class Content extends Fragment {
     private Activity parentActivity;
     private HTML_Loader html_loader;
     private TextView content_text;
+    private int type;
+
+    private static final String content_types[] = {"suplovani", "jidelna", "odkazy"};
 
     public Content() {
         // Nutně prázdný pro třídy dědící Fragment
@@ -39,17 +46,16 @@ public class Content extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_content, container, false);
         content_text = (TextView) rootView.findViewById(R.id.text_content);
 
-        int i = getArguments().getInt(ARG_CONTENT_NUMBER);
-        switch(i){
+        type = getArguments().getInt(ARG_CONTENT_NUMBER);
+        switch(type){
             case 0:
                 content_text.setMovementMethod(new ScrollingMovementMethod());
                 String prefClass = PreferenceManager.getDefaultSharedPreferences(parentActivity).getString("pref_class", "");
-                getPage(getString(R.string.domain)+"?type=suplovani&trida="+prefClass);
+                getPage(getString(R.string.domain)+"?type="+content_types[type]+"&trida="+prefClass);
                 break;
             case 1:
                 content_text.setMovementMethod(new ScrollingMovementMethod());
-                Boolean prefFood = PreferenceManager.getDefaultSharedPreferences(parentActivity).getBoolean("pref_food", false);
-                getPage(getString(R.string.domain)+"?type=jidelna&alergeny="+prefFood);
+                getPage(getString(R.string.domain)+"?type="+content_types[type]);
                 break;
             case 2: //odkazy
                 content_text.setTextSize(35);
@@ -88,6 +94,54 @@ public class Content extends Fragment {
         }
     }
 
-    private void show(String s){ content_text.setText(Html.fromHtml(s)); }
+    private void show(String s) {
+        String result;
+        switch(type){
+            case 0:
+                result = getSuplovani(s);
+                break;
+            case 1:
+                //result = getJidelna(s);
+                break;
+            default:
+                result = "Chyba v požadavku.";
+                break;
+        }
+        content_text.setText(result);
+
+    }
+
+    private String getSuplovani(String s) {
+        try {
+            JSONObject res = new JSONObject(s);
+            if (res.getString("type").equals(content_types[type])) {
+                StringBuilder sb = new StringBuilder();
+                String trida = res.getString("trida");
+                sb.append("<h4>Třída: " + trida + "</h4><br />");
+                JSONArray dny = res.getJSONArray("dny");
+                for (int i = 0; i < dny.length(); i++) {
+                    JSONObject ob = dny.getJSONObject(i);
+                    String den = ob.getString("den");
+                    sb.append("<h5>" + den + "</h5><br />");
+                    String zmeneno = ob.getString("zmeneno");
+
+                    JSONArray hodiny = ob.getJSONArray("hodiny");
+                    for (int j = 0; j < hodiny.length(); j++) {
+                        JSONObject hod = hodiny.getJSONObject(j);
+                        int hodina = hod.getInt("hodina");
+                        String predmet = hod.getString("predmet");
+                        String zmena = hod.getString("zmena");
+                        sb.append("<b>" + hodina + "</b>" + predmet + zmena + "<br />");
+                    }
+                }
+                return sb.toString();
+            } else {
+                return "Chyba pří načítání suplování.";
+            }
+        }catch(JSONException e){
+            return "Chyba pří načítání suplování.";
+        }
+    }
+
 
 }
